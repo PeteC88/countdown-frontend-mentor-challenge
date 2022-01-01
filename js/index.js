dcda244ed485aa9dc774ec9ff7ca100aa7c82f94
PeteC88 <pietrociccarello@gmail.com>
2022-01-01 22:47:11 +0100

    several changes to the mobile layout and added background

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function setEl(type) {
 
     //timer
     let now = new Date().getTime();
-    let endDate = new Date("January 02, 2022 00:00:00");
+    let endDate = new Date("January 02, 2022 11:30:00");
     let diff = endDate - now;
 
     let s = 1000;
