f9039fc124edfa33254e232cfda99e7a72b4c37b
PeteC88 <40523064+PeteC88@users.noreply.github.com>
2022-01-03 15:18:31 +0100

    Add files via upload
    js file modification deleted.

--- js/index.js
+++ js/index.js
@@ -36,11 +36,9 @@ function setEl(type) {
 
     if (type === "seconds") {
         curr = seconds;
-        flipNode.card.classList.remove("active");
-        flipNode.card.classList.add("active");
-        /* if (days !== 0 || hours !== 0 || minutes !== 0) {
+        if (days !== 0 || hours !== 0 || minutes !== 0) {
             flipNode.card.classList.add("active");
-        } */
+        }
     } else if (type === "minutes") {
         curr = minutes;
         if (seconds === 0) {
