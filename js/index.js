9831c82c5f3685c8d45ad5329131d673fee524b9
PeteC88 <40523064+PeteC88@users.noreply.github.com>
2023-02-06 15:06:08 +0100

    Update date

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function setEl(type) {
 
     //timer
     let now = new Date().getTime();
-    let endDate = new Date("October 31, 2022 00:00:00");
+    let endDate = new Date("April 16, 2023 00:00:00");
     let diff = endDate.getTime() - now;
 
 
