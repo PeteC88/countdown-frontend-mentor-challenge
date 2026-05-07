52f0ef88f5141ec6a801351781de3b57b623ec27
PeteC88 <40523064+PeteC88@users.noreply.github.com>
2026-05-07 10:13:52 +0200

    Update countdown

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function setEl(type) {
 
     //timer
     let now = new Date().getTime();
-    let endDate = new Date("August 20, 2025 00:00:00");
+    let endDate = new Date("July 20, 2026 00:00:00");
     let diff = endDate.getTime() - now;
 
 
